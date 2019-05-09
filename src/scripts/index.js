675cd2b1a83818928168ec9d135525e3ef54c72e
Matt Nadolny <mattn@alley.co>
2019-05-09 10:41:18 -0400

    new logo

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,7 @@
 import '../styles/index.scss';
-var logo1 = require('../images/logo-df.png');
+var images = require('../images/logo-df.png');
 var logo2 = require('../images/logo-hsf.png');
 var logo3 = require('../images/logo-tmf.png');
+var logo4 = require('../images/logo-kff.jpg');
 
 var pdf = require('../pdf/legal-clinic-fund-program-guidelines.pdf');
