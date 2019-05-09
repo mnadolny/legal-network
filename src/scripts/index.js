693581acd6d4aa16503279954099631c9398a6a7
Matt Nadolny <mattn@alley.co>
2019-05-09 11:46:18 -0400

    update pdf and date

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,4 +4,4 @@ var logo2 = require('../images/logo-hsf.png');
 var logo3 = require('../images/logo-tmf.png');
 var logo4 = require('../images/logo-kff.jpg');
 
-var pdf = require('../pdf/legal-clinic-fund-program-guidelines.pdf');
+var pdf = require('../pdf/legal-clinic-fund-program-guidelines-1.pdf');
